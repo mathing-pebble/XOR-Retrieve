20dc887c6055c97b013d81fc2bcc83435677a5c1
mathing-pebble <118791131+mathing-pebble@users.noreply.github.com>
2024-07-14 14:23:51 +0900

    Update jax_train.py

--- jax_train.py
+++ jax_train.py
@@ -109,7 +109,10 @@ def main():
     # Handle dataset language properly
     dataset_config = data_args.dataset_name
 
-    train_dataset = datasets.load_dataset(dataset_config, data_args.dataset_language, cache_dir=model_args.cache_dir, data_files=data_files)[data_args.dataset_split]
+
+    dataset_config_name = "full"  
+
+    train_dataset = datasets.load_dataset(dataset_config, dataset_config_name, cache_dir=model_args.cache_dir, data_files=data_files)[data_args.dataset_split]
 
     basic_tokenizer = BasicTokenizer(do_lower_case=False)
     src2tgt = get_dict(data_args.codemix_set)
