a1d5742ce09d4a31ae1fa976d0e6fdd4bf12f2c4
mathing-pebble <118791131+mathing-pebble@users.noreply.github.com>
2024-07-18 02:27:05 +0900

    Update jax_encode.py

--- jax_encode.py
+++ jax_encode.py
@@ -1,14 +1,16 @@
 import logging
 import os
-import pickle
 import sys
+import json
 
 import datasets
 import jax
 import numpy as np
 from flax.training.common_utils import shard
 from jax import pmap
-from arguments import DataArguments, TevatronTrainingArguments as TrainingArguments, ModelArguments
+from arguments import DataArguments
+from arguments import TevatronTrainingArguments as TrainingArguments
+from arguments import ModelArguments
 from data import EncodeCollator, EncodeDataset
 from _datasets.dataset import HFQueryDataset, HFCorpusDataset
 from torch.utils.data import DataLoader
@@ -16,7 +18,8 @@ from tqdm import tqdm
 from flax.training.train_state import TrainState
 from flax import jax_utils
 import optax
-from transformers import (AutoConfig, AutoTokenizer, FlaxAutoModel, HfArgumentParser, TensorType)
+from transformers import (AutoConfig, AutoTokenizer, FlaxAutoModel,
+                          HfArgumentParser, TensorType)
 
 logger = logging.getLogger(__name__)
 
@@ -26,9 +29,6 @@ def main():
         model_args, data_args, training_args = parser.parse_json_file(json_file=os.path.abspath(sys.argv[1]))
     else:
         model_args, data_args, training_args = parser.parse_args_into_dataclasses()
-        model_args: ModelArguments
-        data_args: DataArguments
-        training_args: TrainingArguments
 
     # Setup logging
     logging.basicConfig(
@@ -37,20 +37,20 @@ def main():
         level=logging.INFO if training_args.local_rank in [-1, 0] else logging.WARN,
     )
 
-    if data_args.encode_is_qry:
-        model_path = os.path.join(model_args.model_name_or_path, "query_encoder")
-    else:
-        model_path = os.path.join(model_args.model_name_or_path, "passage_encoder")
-
+    model_path = (
+        model_args.model_name_or_path
+        if not model_args.untie_encoder
+        else f'{model_args.model_name_or_path}/{"query_encoder" if data_args.encode_is_qry else "passage_encoder"}'
+    )
 
     num_labels = 1
     config = AutoConfig.from_pretrained(
-        model_path,
+        model_args.config_name if model_args.config_name else model_path,
         num_labels=num_labels,
         cache_dir=model_args.cache_dir,
     )
     tokenizer = AutoTokenizer.from_pretrained(
-        model_path,
+        model_args.tokenizer_name if model_args.tokenizer_name else model_args.model_name_or_path,
         cache_dir=model_args.cache_dir,
         use_fast=False,
     )
@@ -59,15 +59,27 @@ def main():
 
     text_max_length = data_args.q_max_len if data_args.encode_is_qry else data_args.p_max_len
     if data_args.encode_is_qry:
-        encode_dataset = HFQueryDataset(tokenizer=tokenizer, data_args=data_args,
-                                        cache_dir=data_args.data_cache_dir or model_args.cache_dir)
+        dataset_class = HFQueryDataset
     else:
-        encode_dataset = HFCorpusDataset(tokenizer=tokenizer, data_args=data_args,
-                                         cache_dir=data_args.data_cache_dir or model_args.cache_dir)
-    encode_dataset = EncodeDataset(encode_dataset.process(data_args.encode_num_shard, data_args.encode_shard_index),
-                                   tokenizer, max_len=text_max_length)
+        dataset_class = HFCorpusDataset
 
-    # prepare padding batch (for last nonfull batch)
+    dataset_cache_dir = data_args.data_cache_dir or model_args.cache_dir
+    if not dataset_cache_dir:
+        dataset_cache_dir = os.path.join("/tmp/dataset_cache", data_args.dataset_name.replace("/", "_"))
+    else:
+        dataset_cache_dir = os.path.join(dataset_cache_dir, data_args.dataset_name.replace("/", "_"))
+
+    if not os.path.exists(dataset_cache_dir):
+        os.makedirs(dataset_cache_dir)
+        encode_dataset = dataset_class(tokenizer=tokenizer, data_args=data_args, cache_dir=dataset_cache_dir)
+        encode_dataset = EncodeDataset(encode_dataset.process(data_args.encode_num_shard, data_args.encode_shard_index),
+                                       tokenizer, max_len=text_max_length)
+    else:
+        logger.info("Dataset already downloaded. Reusing the cache.")
+        encode_dataset = dataset_class(tokenizer=tokenizer, data_args=data_args, cache_dir=dataset_cache_dir)
+        encode_dataset = EncodeDataset(encode_dataset.process(data_args.encode_num_shard, data_args.encode_shard_index),
+                                       tokenizer, max_len=text_max_length)
+
     dataset_size = len(encode_dataset)
     padding_prefix = "padding_"
     total_batch_size = len(jax.devices()) * training_args.per_device_eval_batch_size
@@ -94,31 +106,32 @@ def main():
         num_workers=training_args.dataloader_num_workers,
     )
 
-    # craft a fake state for now to replicate on devices
     adamw = optax.adamw(0.0001)
     state = TrainState.create(apply_fn=model.__call__, params=model.params, tx=adamw)
 
     def encode_step(batch, state):
-        input_ids = batch['input_ids']
-        attention_mask = batch['attention_mask']
-        embedding = state.apply_fn(input_ids=input_ids, attention_mask=attention_mask, params=state.params, train=False)[0]
+        embedding = state.apply_fn(**batch, params=state.params, train=False)[0]
         return embedding[:, 0]
-
 
     p_encode_step = pmap(encode_step)
     state = jax_utils.replicate(state)
- 
+
     encoded = []
     lookup_indices = []
 
     for (batch_ids, batch) in tqdm(encode_loader):
         lookup_indices.extend(batch_ids)
-        batch_embeddings = p_encode_step(shard(batch['input_ids']), state)
+        batch_embeddings = p_encode_step(shard(batch["input_ids"]), state)
         encoded.extend(np.concatenate(batch_embeddings, axis=0))
+    
+    output_data = {
+        "encoded_queries": [encoded_item.tolist() for encoded_item in encoded[:dataset_size]],
+        "lookup_indices": lookup_indices[:dataset_size]
+    }
 
-    # Save the embeddings and lookup indices to a file
-    with open(data_args.encoded_save_path, 'wb') as f:
-        pickle.dump((np.array(encoded[:dataset_size]), lookup_indices[:dataset_size]), f)
+    with open(data_args.encoded_save_path, 'w') as f:
+        json.dump(output_data, f)
+
 
 if __name__ == "__main__":
     main()
